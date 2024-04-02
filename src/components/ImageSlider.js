adcfaff1691d7ed058bfdd6411367af0983fd055
jbest75 <jbestsolutions@gmail.com>
2024-04-01 23:14:14 -0600

    setup gh-pages

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,173 @@
+import React, { useState, useRef } from 'react'
+import { SliderData } from './SliderData'
+import { SliderDataMobile } from './SliderData'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft, FaMobileAlt, FaDesktop, FaHatWizard, FaReact, FaLinkedin } from 'react-icons/fa'
+import { GiRaiseZombie, GiCowboyHolster } from "react-icons/gi";
+
+
+const ImageSlider = ({ slides }) => {
+    const [current, setCurrent] = useState(0)
+    // todo - get buttons to not load 2 slides, mobile and desktop, per click
+    // and speed test and optimization
+
+    const [showMobile, setShowMobile] = useState(false)
+    const [showDesktop, setShowDesktop] = useState(false)
+    const [hideShowMobile, setHideShowMobile] = useState(false)
+    const [hideShowDesktop, setHideShowDesktop] = useState(false)
+    const length = slides.length
+    const ref = useRef(null)
+
+    const scrollToSlides = () => {
+        setTimeout(() => {
+            ref.current?.scrollIntoView({ behavior: 'smooth' });
+        }, 25);
+    };
+
+    const nextslide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+    const previousslide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    const nextslideMobile = () => {
+        const length = 11
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+    const previousslideMobile = () => {
+        const length = 11
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    const showMobileWork = () => {
+        setHideShowMobile(!hideShowMobile)
+        setShowMobile(!showMobile)
+        if (showDesktop === true) {
+            setHideShowDesktop(!hideShowDesktop)
+            setShowDesktop(false)
+        }
+        setCurrent(0)
+        scrollToSlides()
+    }
+    const showDesktopWork = () => {
+        setHideShowDesktop(!hideShowDesktop);
+        setShowDesktop(!showDesktop)
+        if (showMobile === true) {
+            setHideShowMobile(!hideShowMobile)
+            setShowMobile(false)
+        }
+        setCurrent(0)
+        scrollToSlides()
+    }
+
+    console.log(current);
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    return (
+        <div className='wrapper'>
+            <h1>Design, Print & Frontend Work
+                <br />
+                <span className='red'>Angular</span> <span className='white'>Html</span> <span className='green'>CSS</span> <span className='blue'>React</span>
+                <p className='small'><b>&copy; Warrior Workshop LLC, Evil Duck Studios & Joshua Best.</b> Screenshots are &copy; by their perspective persons and or companys</p>
+                <img id="headerFlag" src={require("../images/usflag.png")} alt="Made In The USA!" tag="Made In The USA!"></img>
+            </h1>
+            <section id='mobileOrDesktop'>
+                <h2>Check out some of my previous work!</h2>
+                <div id='choiceWrapper'>
+                    <div className={hideShowMobile ? "choiceBtn viewing" : "choiceBtn notViewing"} onClick={showMobileWork}>
+                        <FaMobileAlt className='icon' />
+                        <p>
+                            <b>Web Mobile</b>
+                            <br />
+                            (handful of slides)
+                            <br />
+                            <FaReact className='blue' />
+                        </p>
+                    </div>
+                    <div className={hideShowDesktop ? "choiceBtn viewing" : "choiceBtn notViewing"} onClick={showDesktopWork}>
+                        <FaDesktop className='icon' />
+                        <p>
+                            <b>Web & Design</b>
+                            <br />
+                            (handful of slides)
+                            <br />
+                            <FaReact className='blue' />
+                        </p>
+                    </div>
+                    <a className='choiceBtn notViewing' href='https://jbest75.github.io/RunelordsCardGame/' target='_blank'>
+                        <FaHatWizard  className='icon wizard' />
+                        <p>
+                            <b>My Runlords Card Game App</b>
+                            <br />
+                            (angular 17)
+                        </p>
+                    </a>
+                    <a className='choiceBtn notViewing' href='https://www.thezombietimes.com' target='_blank'>
+                        <GiRaiseZombie className='icon zombie' />
+                        <p>
+                            <b>One of My Sites</b>
+                            <br />
+                            (old - built on jekyll)
+                        </p>
+                    </a>
+                    <a className='choiceBtn notViewing' href='https://www.evilduckstudios.com' target='_blank'>
+                        <GiCowboyHolster className='icon leather' />
+                        <p>
+                            <b>Leather Work</b>
+                            <br />
+                            (leather products, Evil Duck Studios)
+                        </p>
+                    </a>
+                    <a ref={ref} className='choiceBtn notViewing' href='http://www.linkedin.com/in/joshua-best-66b0771a' target='_blank'>
+                        <FaLinkedin className='icon' />
+                        <p>
+                            <b>About Me</b>
+                            <br />
+                            (linkedin profile, contact info, resume, etc...)
+                        </p>
+                    </a>
+                </div>
+            </section>
+
+
+            {/* {showDesktop && ( */}
+            <section id='DesktopSlides' className={hideShowDesktop ? "slider show" : "slider hide"}>
+                <FaArrowAltCircleLeft className="previous-arrow icon" onClick={previousslide} />
+                <FaArrowAltCircleRight className="next-arrow icon" onClick={nextslide} />
+
+                {SliderData.map((slide, index) => {
+                    return (
+                        <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                            <p className='description'>{slide.description}</p>
+                            {index === current && (<img src={slide.image} className='image {' alt={slide.description} title={slide.description} />
+                            )}
+                        </div>
+                    )
+                })}
+            </section>
+            {/* )} */}
+
+            {/* {showMobile && ( */}
+            <section id='mobileSlides' className={hideShowMobile ? "slider show" : "slider hide"}>
+                <FaArrowAltCircleLeft className="previous-arrow icon" onClick={previousslideMobile} />
+                <FaArrowAltCircleRight className="next-arrow icon" onClick={nextslideMobile} />
+
+                {SliderDataMobile.map((slide, index) => {
+                    return (
+                        <div className={index === current ? 'slide forMobile active' : 'slide forMobile'} key={index}>
+                            <p className='description'>{slide.description}</p>
+                            {index === current && (<img src={slide.image} className='image {' alt={slide.description} title={slide.description} />
+                            )}
+                        </div>
+                    )
+                })}
+            </section>
+            {/* )} */}
+        </div>
+    )
+}
+
+export default ImageSlider
---
+++ src/components/SliderData.js
@@ -0,0 +1,405 @@
+export const SliderData = [
+    {
+        image: require('../images/slides/AJ-Logo.jpg'),
+        description: 'Logo design for Synergy Diesel in las Vegas'
+    },
+    {
+        image: require('../images/slides/zombie-times.jpg'),
+        description: 'theZombieTimes.com - Created, designed, built and wrote artircles/content. Built/ran with Jekyll. Designed to work with Mobile too! Click the mobile icon to see it.'
+    },    
+    {
+        image: require('../images/slides/web-1--flawless.jpg'),
+        description: 'Flawless Firearms - Designed and built the front end ontop of .net. Designed to work on Mobile Too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-landing.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - landing. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P2. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-2.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P3. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-3.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P4. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-4.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P5. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-5.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P6. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-6.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P7. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-7.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P8. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-activeNutrition-8.jpg'),
+        description: 'Usana Active Nutrition Quiz - built in Angular - P9. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-court.jpg'),
+        description: 'Design and frontend for Ultimate Fighter Winner. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-vetnet.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/thewalkingdeadblog.jpg'),
+        description: 'One of my blogs (old) that I designed and put content up for.'
+    },
+    {
+        image: require('../images/slides/web-SO.jpg'),
+        description: 'Survival Outlet - Design and frontend for shopify. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-1.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P1. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-2.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P2. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P3 - shopping. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop-navExpanded.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view side nav expanded - shopping. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop-productsLanding.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P4 - shopping. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop-product.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P5 - shopping. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop-toasters.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view toaster notifications. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-3-shop-DropDown.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view cart dropdown. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/_USANA-onlineEnrollment-4-infoCollection.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - desktop view P6. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/afightersdietbook.jpg'),
+        description: 'A Fighters Diet Book - Designed, Edited, Illistrated'
+    },
+    {
+        image: require('../images/slides/flyer-renegade-side.jpg'),
+        description: 'Renegade Class Flyer'
+    },
+    {
+        image: require('../images/slides/invitation.jpg'),
+        description: 'Wedding Reception Invitation Design'
+    },
+    {
+        image: require('../images/slides/marketamericaflyer.jpg'),
+        description: 'Money Merge Account Flyer'
+    },
+    {
+        image: require('../images/slides/syntek-2oz-with-frog.jpg'),
+        description: 'Syntek design - They went with another company for their website but kept my idea/design with the tree frog :)'
+    },
+    {
+        image: require('../images/slides/web-8thwonder_dist-home-3.jpg'),
+        description: ''
+    },
+    {
+        image: require('../images/slides/web-BBB-hullinger.jpg'),
+        description: 'Business Black Books - Designed and frontend'
+    },
+    {
+        image: require('../images/slides/web-BeHuman.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-Reboundfx+Landing+PageV1.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-TOP.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-agelWorx-homepage-redesign.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-agelworx-1.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-agelworx-homepage.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-agilis-homepage2.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-bladenetwork.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-cashFlowSeeker-mockup_4.jpg'),
+        description: 'Design and fronted'
+    },
+    {
+        image: require('../images/slides/web-clubCo-mockup_2.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-coreVital-2.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-freakin-hotties.jpg'),
+        description: 'Design and frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-freakin-mma.jpg'),
+        description: 'Design and Frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-glimpse.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-gunrunners.jpg'),
+        description: 'Design and frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-impact-greetings.jpg'),
+        description: 'Design, Logo and frontend'
+    },
+    {
+        image: require('../images/slides/web-maakoa-biz.jpg'),
+        description: 'Design and frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-mangosteenU-landing-page.jpg'),
+        description: 'Lead Capture Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-mma5-green6-2.jpg'),
+        description: 'MMA 5 design and frontend, built with jQuery on .net'
+    },
+    {
+        image: require('../images/slides/web-mma5-duckies.jpg'),
+        description: 'Design and frontend, made user selectable skins'
+    },
+    {
+        image: require('../images/slides/web-mma5-sunset.jpg'),
+        description: 'Design and frontend, made different skins that would change based on the time of day'
+    },
+    {
+        image: require('../images/slides/web-mockup2.jpg'),
+        description: 'Design and frontend for new home page and layout'
+    },
+    {
+        image: require('../images/slides/web-mockup2-subpage.jpg'),
+        description: 'Design and frontend for new home page and layout - secondary pages'
+    },
+    {
+        image: require('../images/slides/web-monavie-monaLanding_noGrey.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-monavie-the-blend-screenshot2.jpg'),
+        description: 'Design and frontend - when an ingredient is clicked the information about it would load in the center with animations'
+    },
+    {
+        image: require('../images/slides/web-neways-front-office-screen-shot.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-nlscIT.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-noni.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-pinacle-dis-homepage-4.jpg'),
+        description: 'Design and frontend - Distributors homepage'
+    },
+    {
+        image: require('../images/slides/web-pureworks-home-2-2-1.jpg'),
+        description: 'Design and frontend - Distributor homepage'
+    },
+    {
+        image: require('../images/slides/web-purekworks-backoffice.jpg'),
+        description: 'Design and frontend - Back office homepage'
+    },
+    {
+        image: require('../images/slides/web-reboundFx-small-home.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-shammansway.jpg'),
+        description: 'Design - I liked this mockup better than the finalized design :)'
+    },
+    {
+        image: require('../images/slides/webSmall-8.jpg'),
+        description: 'Design and frontend - final from previous slide. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/webSmall-ma-small.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-taylors-site.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-throwdown-yoga.jpg'),
+        description: 'Design and frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/webSmall-afightersdiet.jpg'),
+        description: 'Design and frontend. Designed to work on Mobile too!'
+    },
+    {
+        image: require('../images/slides/web-ufirst-conventions.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/web-weXL-small-home.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/worth-small.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/worth2.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-1.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-2.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-3.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-4.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-7.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-9.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-10.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-11.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-12.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-13.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-14.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-15.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-17.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-19.jpg'),
+        description: 'Design and frontend'
+    },
+    {
+        image: require('../images/slides/webSmall-21.jpg'),
+        description: 'Design and frontend'
+    }
+]
+
+export const SliderDataMobile = [
+    {
+        image: require('../images/slides/zombie-times-mobile.jpg'),
+        description: 'theZombieTimes.com - Mobile View - Created, designed, built and artircles/content by me using Jekyll.'
+    },
+    {
+        image: require('../images/slides/zombie-times-mobile-navigation.jpg'),
+        description: 'theZombieTimes.com - Mobile View - Navigation - Created, designed, built and artircles/content by me using Jekyll.'
+    },
+    {
+        image: require('../images/slides/zombie-times-mobile-search.jpg'),
+        description: 'theZombieTimes.com - Mobile View - Search - Created, designed, built and artircles/content by me using Jekyll.'
+    },
+    {
+        image: require('../images/slides/zombie-times-mobile-categorie-page.jpg'),
+        description: 'theZombieTimes.com - Mobile View - Categorie Page - Created, designed, built and artircles/content by me using Jekyll.'
+    },
+    {
+        image: require('../images/slides/zombie-times-mobile-article.jpg'),
+        description: 'theZombieTimes.com - Mobile View - News Article - Created, designed, built and artircles/content by me using Jekyll.'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-1.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 1'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-2.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 2'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-3-shop-landing.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 3'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-3-shop-product.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 4'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-4-infoCollection.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 5'
+    },
+    {
+        image: require('../images/slides/_USANA-mobile-onlineEnrollment-5-infoCollection.jpg'),
+        description: 'Usana Online Enrollment - design & frontend - Mobile View 6'
+    }
+]
